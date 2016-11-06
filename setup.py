fe5d72a4cff8fe25de0dee7ddb114deedc159021
Remzi Arpaci-Dusseau <remzi.arpacidusseau@gmail.com>
2016-11-06 10:30:50 -0600

    Adding setup.py for pip

---
+++ setup.py
@@ -0,0 +1,16 @@
+# python 
+
+from distutils.core import setup
+setup(
+    name = 'zplot',
+    packages = ['zplot'], 
+    version = '1.0',
+    description = 'A simple graph-creation library',
+    author = 'Remzi H. Arpaci-Dusseau',
+    author_email = 'remzi.arpacidusseau@gmail.com',
+    url = 'https://github.com/z-plot/z-plot', 
+    download_url = 'https://github.com/z-plot/z-plot/tarball/1.0', 
+    keywords = ['plotting', 'graphing', 'postscript', 'svg'], 
+    classifiers = [],
+    )
+
